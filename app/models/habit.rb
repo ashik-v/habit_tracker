c69face1fb9b2a672fd0dc7deb20bcdc735c7f35
Ashik Varghese <ashikv@nulogy.com>
2022-05-31 09:21:39 -0400

    Add model tests to habit

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,3 +1,20 @@
+class DateValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value.is_a?(Date)
+      record.errors.add attribute, (options[:message] || "is not a date")
+    end
+  end
+end
+
 class Habit < ApplicationRecord
   serialize :tracked_dates, Array
+  validate :tracked_dates_type
+
+  def tracked_dates_type
+    tracked_dates.each do |tracked_date|
+      unless tracked_date.is_a?(Date)
+        errors.add(:tracked_dates, "must only contain dates")
+      end
+    end
+  end
 end
--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Habit, type: :model do
+  it "accepts date values for tracked dates" do
+    habit = Habit.new(name: "new habit", start_date: Date.today, tracked_dates: [Date.today])
 
+    expect(habit).to be_valid
+  end
+
+  it "rejects non-date values for tracked dates" do
+    habit = Habit.new(name: "new habit", start_date: Date.today, tracked_dates: ["01/01/2030"])
+
+    expect(habit).to_not be_valid
+  end
 end
