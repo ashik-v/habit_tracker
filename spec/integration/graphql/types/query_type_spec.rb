a0be2f1dc1c10a650de96943b6863f4764860ad4
Ashik Varghese <ashikv@nulogy.com>
2022-07-28 07:51:41 -0600

    Add test for habit query

---
+++ spec/integration/graphql/types/query_type_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType, type: :request do
+  describe "habits query" do
+    it "queries all habits" do
+      habit = Habit.create(name: "sleep on time", tracked_dates: [])
+      another_habit = Habit.create(name: "moisturize", tracked_dates: [Date.parse("2022-07-26")])
+
+      habits_query = <<~GRAPHQL
+        query {
+          habits {
+            name
+            trackedDates
+          }
+        }
+      GRAPHQL
+      post "/graphql", params: { query: habits_query }
+
+      result = JSON.parse(response.body)
+      expected_result = { "data" => { "habits" => [ { "name" => habit.name, "trackedDates" => habit.tracked_dates }, { "name" => another_habit.name, "trackedDates" => another_habit.tracked_dates.map(&:to_s) } ] } }
+      expect(result).to eq(expected_result)
+      expect(response).to be_successful
+    end
+  end
+end
